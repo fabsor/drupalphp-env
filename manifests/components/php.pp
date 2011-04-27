375b5b9ea1799fefec9fe3bbf18b995fe54ef3ab
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2011-04-27 21:19:55 +0200

    handle php.in

--- manifests/components/php.pp
+++ manifests/components/php.pp
@@ -1,7 +1,16 @@
 class php {
   $packages = [ "php5", "php5-mysql", "php5-gd", "php-apc", "php5-curl", "php5-dev", "php5-dbg"]
+  # PHP.ini file.
+  file { "/etc/php5/apache2/php.ini":
+    owner => root,
+    group => root,
+    mode => 0444,
+    source => "/srv/config/php/php-apache.ini"
+  }
 
   package { $packages:
     ensure => installed,
   }
+
+  
 }
